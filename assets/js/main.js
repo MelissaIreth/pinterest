a46ce5ea2f96156882d3d199bcac495a4f4e8df6
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-28 12:59:02 -0400

    img + descrption

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	yeison.forEach(function(cv){
-		$('.show-imgs').append('<li><img src="assets/img/'+cv.image_url+'"></li>')
+		$('.show-imgs').append('<li><img src="assets/img/'+cv.image_url+'"><h2>'+cv.title+'<span> <i class="fa fa-thumb-tack" aria-hidden="true"></i> 36,6k <i class="fa fa-check" aria-hidden="true"> 6</i></span></h2><p>'+cv.description+'<h4><span class="circle">M </span>'+cv.user+' # '+cv.hashtag+'</h4></li>')
 	})
 })
