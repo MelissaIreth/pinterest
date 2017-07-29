b61b8d5627c21d61f1f833157a13c5938db978b9
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-28 21:59:42 -0400

    all 4today

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,10 @@ $(document).ready(function() {
 		var img = '<img src="assets/img/'+ yeison[i].image_url +'">';
 		var title = '<h4>'+yeison[i].title+'<span> <i class="fa fa-thumb-tack" aria-hidden="true"></i> 36,6k <i class="fa fa-check" aria-hidden="true"> 6</i></span></h4>';
 		var description = '<p>'+yeison[i].description+'</p>';
-		var user_hash = '<h4><span class="circle">M </span>'+yeison[i].user+' # '+yeison[i].hashtag+'</h4></li>';
+		var user_hash = '<h4><span class="circle">M </span>'+yeison[i].user+' #'+yeison[i].hashtag+'</h4></li>';
 		
+		$('#hashtags').append('<div class="all-hash"><a href="#">'+yeison[i].hashtag+'</a></div>');
+
 		$('.show-imgs').append('<div class="caja" id="'+id+'">'+ img +'<div class="all-desc">'+ title+ description + user_hash +'</div></div>');
 		/*prueba para modal*/
 		$('#' + id).click(function(){
@@ -22,7 +24,7 @@ $(window).scroll(function() {
 		var img = '<img src="assets/img/'+ yeison[i].image_url +'">';
 		var title = '<h4>'+yeison[i].title+'<span> <i class="fa fa-thumb-tack" aria-hidden="true"></i> 36,6k <i class="fa fa-check" aria-hidden="true"> 6</i></span></h4>';
 		var description = '<p>'+yeison[i].description+'</p>';
-		var user_hash = '<h4><span class="circle">M </span>'+yeison[i].user+' # '+yeison[i].hashtag+'</h4>';
+		var user_hash = '<h4><span class="circle">M </span>'+yeison[i].user+' #'+yeison[i].hashtag+'</h4>';
 		
 		$('.show-imgs').append('<div class="caja" id="'+id+'">'+ img +'<div class="all-desc">'+ title + description + user_hash +'</div></div>');
 	}
