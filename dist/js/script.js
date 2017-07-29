84bb9771e0afc5da8a485b58eea744174746ab2e
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-28 21:11:15 -0400

    add nav + style

--- dist/js/script.js
+++ dist/js/script.js
@@ -10258,12 +10258,12 @@ $(document).ready(function() {
 	for (var i = 0; i < 20; i++) {
 		var id = yeison[i].id;
 		var img = '<img src="assets/img/'+ yeison[i].image_url +'">';
-		var title = '<h2>'+yeison[i].title+'<span> <i class="fa fa-thumb-tack" aria-hidden="true"></i> 36,6k <i class="fa fa-check" aria-hidden="true"> 6</i></span></h2>';
+		var title = '<h4>'+yeison[i].title+'<span> <i class="fa fa-thumb-tack" aria-hidden="true"></i> 36,6k <i class="fa fa-check" aria-hidden="true"> 6</i></span></h4>';
 		var description = '<p>'+yeison[i].description+'</p>';
 		var user_hash = '<h4><span class="circle">M </span>'+yeison[i].user+' # '+yeison[i].hashtag+'</h4></li>';
 		
-		$('.show-imgs').append('<li id="'+id+'">'+ img +'<div class="all-desc">'+ title+ description + user_hash +'</div></li>');
-		
+		$('.show-imgs').append('<div class="caja" id="'+id+'">'+ img +'<div class="all-desc">'+ title+ description + user_hash +'</div></div>');
+		/*prueba para modal*/
 		$('#' + id).click(function(){
         	alert("Titulo: "+ id)
         });
@@ -10275,11 +10275,11 @@ $(window).scroll(function() {
       for (var i = 21; i < 40; i++) {
       	var id = yeison[i].id;
 		var img = '<img src="assets/img/'+ yeison[i].image_url +'">';
-		var title = '<h2>'+yeison[i].title+'<span> <i class="fa fa-thumb-tack" aria-hidden="true"></i> 36,6k <i class="fa fa-check" aria-hidden="true"> 6</i></span></h2>';
+		var title = '<h4>'+yeison[i].title+'<span> <i class="fa fa-thumb-tack" aria-hidden="true"></i> 36,6k <i class="fa fa-check" aria-hidden="true"> 6</i></span></h4>';
 		var description = '<p>'+yeison[i].description+'</p>';
 		var user_hash = '<h4><span class="circle">M </span>'+yeison[i].user+' # '+yeison[i].hashtag+'</h4>';
 		
-		$('.show-imgs').append('<li id="'+id+'">'+ img +'<div class="all-desc">'+ title + description + user_hash +'</div></li>');
+		$('.show-imgs').append('<div class="caja" id="'+id+'">'+ img +'<div class="all-desc">'+ title + description + user_hash +'</div></div>');
 	}
    }
 });
